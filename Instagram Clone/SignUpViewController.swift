e961b87334881620bc1cc8f6783feb32ef729b9c
kioko <kiokotomas@gmail.com>
2016-02-08 22:46:17 +0300

    Add function to hide and show keyboard

--- Instagram Clone/SignUpViewController.swift
+++ Instagram Clone/SignUpViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
@@ -25,7 +25,7 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var profileImageView: UIImageView!
     
     //ScrollView
-    @IBOutlet weak var srollView: UIScrollView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     //Reset defaul srollView size
     var scrollViewHeight : CGFloat = 0
@@ -36,21 +36,60 @@ class SignUpViewController: UIViewController {
     //Main Default function
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //ScrollView Frame size
+        scrollView.frame = CGRectMake(0, 0, self.view.frame.width ,self.view.frame.height)
+        scrollView.contentSize.height = self.view.frame.height
+        scrollViewHeight = scrollView.frame.size.height
+        
+        //Check notificaiton if keyboard is showing
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "showKeyboard:", name: UIKeyboardWillShowNotification, object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "hideKeyboard:", name: UIKeyboardWillHideNotification, object: nil)
+        
+        let hideTap = UITapGestureRecognizer(target: self, action: "hideKeyboardType:")
+        hideTap.numberOfTapsRequired = 1
+        self.view.userInteractionEnabled = true
+        self.view.addGestureRecognizer(hideTap) //add teh gesture the view
+        
     }
-
+    
+    //Hide the keyboard if the user taps outside the keyboard
+    func hideKeyboardType(recognizer: UITapGestureRecognizer){
+        self.view.endEditing(true)
+    }
+    
+    //Show keyboard and align it with the view
+    func showKeyboard(notification: NSNotification){
+        
+        //Define Keyboard size
+        keyboard = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey]!.CGRectValue)!
+        
+        //Define the animation to mive the UI up
+        UIView.animateWithDuration(0.4) { () -> Void in
+            self.scrollView.frame.size.height = self.scrollViewHeight - self.keyboard.height
+        }
+    }
+    
+    //Hide the keyboard
+    func hideKeyboard(notification: NSNotification){
+        
+        //Define the animation to mive the UI down
+        UIView.animateWithDuration(0.4) { () -> Void in
+            self.scrollView.frame.size.height = self.view.frame.height
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-
     @IBAction func signUpAction(sender: AnyObject) {
     }
     
     @IBAction func cancelAction(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
+    
 }
