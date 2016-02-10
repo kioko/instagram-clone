6eba159bbba0dfd113681e3f925717a022dd4f02
kioko <kiokotomas@gmail.com>
2016-02-10 21:49:23 +0300

    Delete unused view controller

--- Instagram Clone/ViewController.swift
+++ Instagram Clone/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        fetchProductViaID()
+//        createProduct()
+      
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func createProduct(){
+        let productsObject = PFObject(className: "Products")
+        
+        productsObject["name"] = "pizza"
+        productsObject["description"] = "Chicken Tikka"
+        productsObject["price"] = 900
+        
+        productsObject.saveInBackgroundWithBlock {
+            (success: Bool, error: NSError?) -> Void in
+            
+            if success == true{
+                print("Object has been saved with ID: \(productsObject.objectId)")
+            }else{
+                print(error)
+            }
+        }
+    }
+    
+    func fetchProductViaID(){
+        let query = PFQuery(className: "Products")
+        
+        query.getObjectInBackgroundWithId("HO8M5N9Z63", block: { (object: PFObject?, error: NSError?) -> Void in
+        
+            if error != nil{
+                print(error)
+            }else if let product = object{
+               
+                product["description"] = "Peri Peri"
+                product["price"] = 1400
+                
+                product.saveInBackground()
+            }
+        })
+    }
+    
+    
 }
 
