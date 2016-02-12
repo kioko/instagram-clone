9bf2826713f74776784a204e58435d0056e36aa6
kioko <kiokotomas@gmail.com>
2016-02-12 22:32:55 +0300

    Create cell view class

---
+++ Instagram Clone/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Instagram Clone
+//
+//  Created by Kioko on 12/02/2016.
+//  Copyright © 2016 Thomas Kioko. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
