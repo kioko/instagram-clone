f397840ee04a5858d39be0904ea16660722130ac
kioko <kiokotomas@gmail.com>
2016-02-16 20:02:58 +0300

    Hide show keyboard

--- Instagram Clone/SignInViewController.swift
+++ Instagram Clone/SignInViewController.swift
@@ -15,11 +15,26 @@ class SignInViewController: UIViewController {
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    //Keyboard frame size
+    var keyboard = CGRect()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //set the label font style
         instagramLabel.font = UIFont(name: "Pacifico", size: 50)
+        
+        
+        //Check notificaiton if keyboard is showing
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "showKeyboard:", name: UIKeyboardWillShowNotification, object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "hideKeyboard:", name: UIKeyboardWillHideNotification, object: nil)
+        
+        //Hide keyboard tap guesture
+        let hideTap = UITapGestureRecognizer(target: self, action: "hideKeyboardType:")
+        hideTap.numberOfTapsRequired = 1
+        self.view.userInteractionEnabled = true
+        self.view.addGestureRecognizer(hideTap) //add the gesture the view
     }
     
     override func didReceiveMemoryWarning() {
@@ -74,6 +89,33 @@ class SignInViewController: UIViewController {
         //Display the alert dialog
         self.presentViewController(alertDialog, animated: true, completion: nil)
     }
+    
+    //Hide the keyboard if the user taps outside the keyboard
+    func hideKeyboardType(recognizer: UITapGestureRecognizer){
+        self.view.endEditing(true)
+    }
+    
+    //Show keyboard and align it with the view
+    func showKeyboard(notification: NSNotification){
+        
+        //Define Keyboard size
+        keyboard = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey]!.CGRectValue)!
+        
+        //Define the animation to mive the UI up
+        UIView.animateWithDuration(0.4) { () -> Void in
+//            self.scrollView.frame.size.height = self.scrollViewHeight - self.keyboard.height
+        }
+    }
+    
+    //Hide the keyboard
+    func hideKeyboard(notification: NSNotification){
+        
+        //Define the animation to mive the UI down
+        UIView.animateWithDuration(0.4) { () -> Void in
+//            self.scrollView.frame.size.height = self.view.frame.height
+        }
+    }
+
     
     
 }
