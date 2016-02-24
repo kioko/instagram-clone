cf024005ef8ea9ef8e459cfc860cc61b5d911685
kioko <kiokotomas@gmail.com>
2016-02-24 21:08:42 +0300

    add functionality to follow and unfollow a user.

--- Instagram Clone/HeaderCollectionReusableView.swift
+++ Instagram Clone/HeaderCollectionReusableView.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import Parse
 
 class HeaderCollectionReusableView: UICollectionReusableView {
-        
+    
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var bioTextView: UITextView!
@@ -21,6 +22,53 @@ class HeaderCollectionReusableView: UICollectionReusableView {
     @IBOutlet weak var postTitleLabel: UILabel!
     @IBOutlet weak var followingTitleLabel: UILabel!
     @IBOutlet weak var followersTitleLabel: UILabel!
+    @IBOutlet weak var buttonFollow: UIButton!
+    
+    @IBAction func buttonFollowAction(sender: AnyObject) {
+        let followStateTitle = buttonFollow.titleForState(.Normal)
+        
+        if followStateTitle == "Follow"{
+            let object = PFObject(className: "Followers")
+            object["follow"] = PFUser.currentUser()?.username
+            object["following"] = guestUserName.last!
+            
+            object.saveInBackgroundWithBlock({ (success:Bool, error:NSError?) -> Void in
+                
+                if success{
+                    //Update the state of the button.
+                    self.buttonFollow.setTitle("Following", forState: UIControlState.Normal)
+                    self.buttonFollow.backgroundColor = UIColor(rgba: "#4dc247")
+                }else{
+                    print(error?.localizedDescription)
+                }
+            })
+        }else{
+            
+            let followersQuery = PFQuery(className: "Followers")
+            followersQuery.whereKey("following", equalTo: PFUser.currentUser()!.username!)
+            followersQuery.whereKey("following", equalTo: guestUserName.last!)
+            
+            followersQuery.findObjectsInBackgroundWithBlock ({ (objects: [PFObject]?, error: NSError?) -> Void in
+                
+                if error ==  nil{
+                    for object in objects!{
+                        object.deleteInBackgroundWithBlock({ (success: Bool, error: NSError?) -> Void in
+                            
+                            if success{
+                                //Update the state of the button.
+                                self.buttonFollow.setTitle("Follow", forState: UIControlState.Normal)
+                                self.buttonFollow.backgroundColor = UIColor.lightGrayColor()
+                            }else{
+                                print(error?.localizedDescription)
+                            }
+                        })
+                    }
+                }else{
+                    print(error?.localizedDescription)
+                }
+            })
+        }
+    }
     
     @IBAction func editProfileAction(sender: AnyObject) {
     }
