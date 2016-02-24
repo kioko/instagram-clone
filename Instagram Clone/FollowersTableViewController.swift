0b24455ac99d411b4356bcd7d891dfdc855d6901
kioko <kiokotomas@gmail.com>
2016-02-24 21:12:29 +0300

    Make cell item clickable and navigate to the respective view, delete unused code.

--- Instagram Clone/FollowersTableViewController.swift
+++ Instagram Clone/FollowersTableViewController.swift
@@ -157,60 +157,32 @@ class FollowersTableViewController: UITableViewController {
                 cell.followingState.backgroundColor = .lightGrayColor()
             }else{
                 cell.followingState.setTitle("Following", forState: UIControlState.Normal)
-                cell.followingState.backgroundColor = UIColor(rgba: "#4dc247")    
-
+                cell.followingState.backgroundColor = UIColor(rgba: "#4dc247")
+                
             }
         }) //End of query execution
-        
-        
         
         return cell
     }
     
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return false if you do not want the specified item to be editable.
-    return true
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let cell = tableView.cellForRowAtIndexPath(indexPath) as! FollowersCell
+        
+        //If user tapped on himself navigate to home
+        if cell.followerUserNameLabel.text! == PFUser.currentUser()!.username {
+            let homeStoryBoard = self.storyboard?.instantiateViewControllerWithIdentifier("ProfileViewController") as! ProfileViewController
+            self.navigationController?.pushViewController(homeStoryBoard, animated: true)
+        }else{
+            
+            //Set the userName
+            guestUserName.append(cell.followerUserNameLabel.text!)
+            
+            let userStoryBoard = self.storyboard?.instantiateViewControllerWithIdentifier("UserProfileUCollectionViewController") as! UserProfileUCollectionViewController
+            self.navigationController?.pushViewController(userStoryBoard, animated: true)
+        }
+        
     }
-    */
     
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-    if editingStyle == .Delete {
-    // Delete the row from the data source
-    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-    } else if editingStyle == .Insert {
-    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }
-    }
-    */
-    
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-    
-    }
-    */
-    
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return false if you do not want the item to be re-orderable.
-    return true
-    }
-    */
-    
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
-    }
-    */
     
 }
